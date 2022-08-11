8023f68204674a91705811216a63a7965f29acb6
Soroush Mahdavi <soroush.mahdavi01@gmail.com>
2022-08-11 09:33:11 -0700

    Add usersHand class, try to figure out how to calculate total for 2 aces

--- 21BlackJack.py
+++ 21BlackJack.py
@@ -1,17 +1,28 @@
 ################################ IMPORTS ###################################
 
 from classes import *
-import pygame
-import sys
-import random
+import pygame, sys ,random
+pygame.init()
+pygame.font.init()
+gameTrue = pygame.get_init()
+print(gameTrue)
 
 ###############################################################
 
-Deck = 4*["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-DealerHidden = random.choice(Deck)
-DealerShown = random.choice(Deck)
+deck = 4*["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+dealerHidden = random.choice(deck)
+dealerShown = random.choice(deck)
+userFirstCard = random.choice(deck)
+userSecondCard = random.choice(deck)
 
 ############################ FUNCTIONS #####################################
 
-dealer = dealersHand(DealerHidden, DealerShown)
+dealer = dealersHand(dealerHidden, dealerShown)
 print(dealer)
+user = usersHand(userFirstCard, userSecondCard)
+print(user)
+dealerHidden = random.choice(deck)
+dealerShown = random.choice(deck)
+dealer = dealersHand(dealerHidden, dealerShown)
+print(dealer)
+
--- classes.py
+++ classes.py
@@ -6,15 +6,50 @@ class dealersHand():
         self.__dealers_total = 0
         self.__dealer_hidden = hidden
         self.__dealer_shown = shown
-    def set_dealer_hand(self, hidden, shown):
+    def set_dealer_hand(self, score, hidden, shown):
+        self.__dealers_total = score
         self.__dealer_hidden = hidden
         self.__dealer_shown = shown
+    def get_dealers_total(self):
+        if self.__dealer_hidden in ["J", "Q", "K"]:
+            self.__dealer_hidden = 10
+        if self.__dealer_shown in ["J", "Q", "K"]:
+            self.__dealer_shown = 10
+        if self.__dealer_hidden == "A": 
+            if 11 + self.__dealer_shown > 21:
+                return 1 + self.__dealer_shown
+            else:
+                pass
+            
+        
+        return self.__dealers_total
     def get_dealer_hidden(self):
         return self.__dealer_hidden
     def get_dealer_shown(self):
         return self.__dealer_shown
     def __str__(self):
-        return f"The Dealers hand is a {self.__dealer_hidden} and a {self.__dealer_shown}."
+        return f"The Dealers hand shows a {self.__dealer_shown} but hides a {self.__dealer_hidden}"
+    
     
          
+class usersHand():
+    def __init__(self, userFirstCard, userSecondCard):
+        self.__users_total = 0
+        self.__user_first_card = userFirstCard
+        self.__user_second_card = userSecondCard
+    def set_user_hand(self, score, userFirstCard, userSecondCard):
+        self.__users_total = score
+        self.__user_first_card = userFirstCard
+        self.__user_second_card = userSecondCard
+    def get_users_total(self):
+        return self.__users_total
+    def get_user_first_card(self):
+        return self.__user_first_card
+    def get_user_second_card(self):
+        return self.__user_second_card
+    def __str__(self):
+        return f"You have a {self.__user_first_card} and a {self.__user_second_card}"
+    
+        
+        
         
